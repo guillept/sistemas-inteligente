a42218bf8a674d74c36a33fc4eaed882f67e7f76
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2015-05-19 21:59:19 +0300

    Keep track of entity types in world.

--- entities/world.py
+++ entities/world.py
@@ -1,4 +1,7 @@
 from entities.drawable_entity import DrawableEntity
+from entities.mars_base import MarsBase
+from entities.obstacle import Obstacle
+from entities.rock import Rock
 
 
 class World(DrawableEntity):
@@ -7,11 +10,23 @@ class World(DrawableEntity):
     def __init__(self, width, height):
         self.width = width
         self.height = height
+
         self.entities = []
+        self.rocks = []
+        self.obstacles = []
+        self.mars_base = None
 
     def draw(self, canvas):
         canvas.configure(background=self.COLOR)
 
     def add_entity(self, entity):
         assert isinstance(entity, DrawableEntity)
+
         self.entities.append(entity)
+
+        if isinstance(entity, Rock):
+            self.rocks.append(entity)
+        elif isinstance(entity, Obstacle):
+            self.obstacles.append(entity)
+        elif isinstance(entity, MarsBase):
+            self.mars_base = entity
